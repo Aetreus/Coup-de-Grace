6e3093a6be08649e16dfcc5e977d8c94063dbab7
Staeliam Schipper-Reyes <staeliam@gmail.com>
2018-02-17 13:15:21 -0500

    Eliminated static instability issues with FM.

--- Assets/FlightBehavior.cs
+++ Assets/FlightBehavior.cs
@@ -16,14 +16,14 @@ public class FlightBehavior : MonoBehaviour {
     public float maxThrust = 60000;
 
     //Pitching moment coefficient is an aircraft's tendency to center itself vertically(i.e. to fly in the direction it's pointing)
-    public float pitchingMomentCoeffIntercept = 0.05F;//Positive values means the plane has a certain amount of tendency to fly nose-up(it's nose is pulled up when flying straight).
+    public float pitchingMomentCoeffIntercept = 0.02F;//Positive values means the plane has a certain amount of tendency to fly nose-up(it's nose is pulled up when flying straight).
 
     public float pitchingMomentCoeffSlope = -0.01F;//This must be negative for stability.
 
     //Yaw moment coefficient is similar, but makes the aircraft stable in yaw instead of pitch.
     public float yawMomentCoeffIntercept = 0.0F;//Should be zero so that the aircraft faces the direction it is flying.
 
-    public float yawMomentCoeffSlope = -0.05F;//This value must be negative, but its magnitude is less important.
+    public float yawMomentCoeffSlope = -0.15F;//This value must be negative, but its magnitude is less important.
 
     private float elevatorSetting = 0;//These values are the current settings of the control surfaces, from 1.0 to -1.0
 
@@ -122,24 +122,25 @@ public class FlightBehavior : MonoBehaviour {
         rb.AddForce(thrust, ForceMode.Force);
 
         //AoA is the portion of the angle between facing and velocity vector that is in the plane between the up and forward vectors of facing
-        AoA = Vector3.Angle(rb.velocity, Vector3.ProjectOnPlane(rb.transform.forward,Vector3.Cross(rb.transform.forward,rb.transform.up)));
+        AoA = Vector3.Angle(rb.transform.forward, Vector3.ProjectOnPlane(rb.velocity,Vector3.Cross(rb.transform.forward,rb.transform.up)));
         if(Vector3.Dot(Vector3.Cross(rb.velocity,rb.transform.forward),Vector3.Cross(rb.transform.forward,rb.transform.up)) < 0)//Get the sign of the angle difference
         {
             AoA = -AoA;
         }
 
         //
-        sideslip = Vector3.Angle(rb.velocity, Vector3.ProjectOnPlane(rb.transform.forward, Vector3.Cross(rb.transform.right, rb.transform.forward)));
-        if (Vector3.Dot(Vector3.Cross(rb.velocity, rb.transform.forward), Vector3.Cross(rb.transform.right, rb.transform.up)) > 0)//Get the sign of the angle difference
+        sideslip = Vector3.Angle(rb.transform.forward, Vector3.ProjectOnPlane(rb.velocity, Vector3.Cross(rb.transform.right, rb.transform.forward)));
+        if (Vector3.Dot(Vector3.Cross(rb.velocity, rb.transform.forward), Vector3.Cross(rb.transform.right, rb.transform.forward)) < 0)//Get the sign of the angle difference
         {
             sideslip = -sideslip;
         }
 
         float liftMagnitude = (1F / 2) * airDensity * wingArea * (float)Math.Pow((double)rb.velocity.magnitude,2) * CalculateLiftCoeff();
-        Vector3 lift = new Vector3(0, liftMagnitude, 0);//TODO:Should be seperating lift from induced drag. 
+        Vector3 liftDirection = Vector3.Cross(rb.velocity, rb.transform.right).normalized;
+        Vector3 lift = liftDirection * liftMagnitude;
         rb.AddForce(lift, ForceMode.Force);
 
-        float dragMagnitude = (1F / 2) * airDensity * wingArea * (float)Math.Pow((double)rb.velocity.magnitude,2) * dragCoeff;
+        float dragMagnitude = (1F / 2) * airDensity * wingArea * (float)Math.Pow((double)rb.velocity.magnitude, 2) * CalculateDragCoeff();
         Vector3 drag = rb.velocity.normalized * -1 * dragMagnitude;
         rb.AddForce(drag, ForceMode.Force);
 
@@ -168,22 +169,29 @@ public class FlightBehavior : MonoBehaviour {
 
     float CalculateLiftCoeff()
     {
-        if(AoA > 25)//Very lazy stall condition
-        {
-            return 2.4F - 0.11F * (AoA - 25);
-        }
-        else if(AoA > 20)
-        {
-            return 2.4F;
-        }
-        else if(AoA < -10)
-        {
-            return -0.9F;
-        }
-        else
+        if (AoA >= -20 && AoA < 20)//Still lazy but better stall condition.
         {
             return 0.11F * AoA + 0.2F;
         }
+        else if (AoA >= 20 && AoA < 25)
+        {
+            return 2.4F;
+        }
+        else if (AoA < -20 && AoA >= -41)
+        {
+            return -2.4F - 0.11F * (AoA + 20);
+        }
+        else if (AoA >= 25 && AoA < 46)
+        {
+            return 2.4F - 0.11F * (AoA - 25);
+        }
+        else
+            return 0.0F;
+    }
+
+    float CalculateDragCoeff()
+    {
+        return dragCoeff;
     }
 
     float CalculatePitchingCoeff()
--- Assets/PlayerControlBehavior.cs
+++ Assets/PlayerControlBehavior.cs
@@ -14,13 +14,30 @@ public class PlayerControlBehavior : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.DownArrow))
+		if(Input.GetKey(KeyCode.DownArrow))
         {
             fb.SetElevator(0.5F);
         }
-        else if(Input.GetKeyDown(KeyCode.UpArrow))
+        else if(Input.GetKey(KeyCode.UpArrow))
         {
             fb.SetElevator(-0.5F);
         }
-	}
+        else
+        {
+            fb.SetElevator(0.0F);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            fb.SetAileron(0.5F);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            fb.SetAileron(-0.5F);
+        }
+        else
+        {
+            fb.SetAileron(0.0F);
+        }
+    }
 }
