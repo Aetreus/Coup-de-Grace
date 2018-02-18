9245eaeab94c3cb600be68c46809c18fc85be099
Staeliam Schipper-Reyes <staeliam@gmail.com>
2018-02-18 06:33:46 -0500

    Artificial horizon commit.

---
+++ Assets/HorizonController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizonController : MonoBehaviour {
+
+    public GameObject reference;
+
+    public Vector3 translateBase;//Translation amount corresponding to 1 degree pitch
+
+    private Vector3 baseLoc;
+
+    // Use this for initialization
+    void Start () {
+        baseLoc = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float pitch = Vector3.Angle(Vector3.ProjectOnPlane(reference.transform.forward, new Vector3(0, 1, 0)), reference.transform.forward);
+        if(Vector3.Dot(Vector3.Cross(Vector3.ProjectOnPlane(reference.transform.forward, new Vector3(0, 1, 0)),reference.transform.forward),Vector3.Cross(reference.transform.forward,new Vector3(0,1,0))) > 0)
+        {
+            pitch = -pitch;
+        }
+
+        transform.localPosition = baseLoc + translateBase * pitch;
+    }
+}
---
+++ Assets/HorizonRollController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizonRollController : MonoBehaviour {
+
+    public GameObject reference;
+    public Vector3 rotateAxis = new Vector3(0, 1, 0);
+
+    private Quaternion baseRotation;
+
+	// Use this for initialization
+	void Start () {
+        baseRotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //float pitch = Vector3.Angle(Vector3.ProjectOnPlane(reference.transform.forward, new Vector3(0, 1, 0)), reference.transform.forward);
+
+        float roll = Vector3.Angle(reference.transform.up, Vector3.ProjectOnPlane(new Vector3(0, 1, 0), Vector3.Cross(reference.transform.up, reference.transform.right)));
+        if(Vector3.Dot(Vector3.Cross(reference.transform.up, Vector3.ProjectOnPlane(new Vector3(0, 1, 0), Vector3.Cross(reference.transform.up, reference.transform.right))),Vector3.Cross(reference.transform.up, reference.transform.right)) < 0)
+        {
+            roll = -roll;
+        }
+        transform.rotation = baseRotation * Quaternion.AngleAxis(roll, rotateAxis);
+	}
+}
