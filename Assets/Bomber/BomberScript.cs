837630c4dfee10e49793c331461db03fe2327160
Alexander Christner <christner.alex@gmail.com>
2018-03-04 11:03:55 -0500

    work on bomber script and missile script

---
+++ Assets/Bomber/BomberScript.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BomberScript : MonoBehaviour {
+
+    public GameObject missile;
+    public Vector3 travel_direction;
+    public float speed;
+    
+    public GameObject bomb;
+    public GameObject bomb_target;
+
+    private Vector3 velocity;
+    
+    public float drop_frequency;
+    public float bomb_drop_offset;
+    private float drop_timer;
+
+    public float missile_fire_angle;
+    public float missile_chance;
+    private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        transform.forward = travel_direction.normalized;
+        velocity = travel_direction.normalized * speed;
+
+        player = GameObject.FindGameObjectWithTag("player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        MoveUpdate();
+
+        if (drop_timer <= 0 )
+        {
+            CheckBombDrop();
+        }
+        else
+        {
+            drop_timer -= Time.deltaTime;
+        }
+
+        float angle = Vector3.Angle(transform.forward, player.transform.position - transform.position);
+        if(angle <= missile_fire_angle && Random.Range(0.0f, 100.0f) <= missile_chance * Time.deltaTime)
+        {
+            FireMissile();
+        }
+    }
+
+    void MoveUpdate()
+    {
+        transform.Translate(velocity * Time.deltaTime);
+    }
+
+    void CheckBombDrop()
+    {
+        float altitude = Mathf.Abs(bomb_target.transform.position.y - transform.position.y);
+
+        float fallTime = Mathf.Sqrt(altitude / Physics.gravity.magnitude);
+
+        float horiz_dist = (new Vector3(velocity.x, 0, velocity.z)).magnitude * fallTime;
+
+        if (horiz_dist <= bomb_drop_offset)
+        {
+            DropBomb();
+
+            drop_timer = drop_frequency;
+        }
+    }
+
+    void DropBomb()
+    {
+
+    }
+
+    void FireMissile()
+    {
+
+    }
+}
---
+++ Assets/HomingMissile/PropNav.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropNav : MonoBehaviour {
+
+    public float N = 3;
+
+    private GameObject target = null;
+    private Vector3 last_pos;
+    private Vector3 target_last_pos;
+
+    private Vector3 latex = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+        last_pos = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (target)
+        {
+            Vector3 range_new = target.transform.position - transform.position;
+            Vector3 range_old = target_last_pos - last_pos;
+            range_new.Normalize();
+            range_old.Normalize();
+
+            Vector3 LOS_Delta;
+            float LOS_Rate;
+            if (range_old.magnitude == 0)
+            {
+                LOS_Delta = range_new - range_old;
+                LOS_Rate = LOS_Delta.magnitude;
+            }
+            else
+            {
+                LOS_Delta = range_new - range_old;
+                LOS_Rate = LOS_Delta.magnitude;
+            }
+
+            float closing_vel = -LOS_Rate;
+
+            float Nt = Physics.gravity.magnitude;
+            latex = range_new * N * closing_vel * LOS_Rate + LOS_Delta * Nt * N * 0.5f;
+
+            target_last_pos = target.transform.position;
+            last_pos = transform.position;
+        }
+        else
+        {
+            latex = Vector3.zero;
+        }
+    }
+
+    public GameObject Target
+    {
+        get
+        {
+            return target;
+        }
+        set
+        {
+            target = value;
+            target_last_pos = target.transform.position;
+        }
+    }
+
+    public Vector3 Latex
+    {
+        get
+        {
+            return latex;
+        }
+    }
+}
