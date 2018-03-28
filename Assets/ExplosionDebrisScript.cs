100ed1b18f5f85eb82daf88bd2dcb2cce668f1c0
Brandon Ip <Chupalika@gmail.com>
2018-03-27 22:36:58 -0400

    terrain, skybox, explosion

---
+++ Assets/ExplosionDebrisScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionDebrisScript : MonoBehaviour {
+	public float gravity;
+	public float lifetime;
+	private float countdown;
+
+	// Use this for initialization
+	void Start () {
+		countdown = lifetime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		countdown -= 1;
+		if (countdown == 0) {
+			Destroy(gameObject);
+		}
+		gameObject.GetComponent<Rigidbody>().AddForce(Vector3.down * gravity);
+	}
+}
