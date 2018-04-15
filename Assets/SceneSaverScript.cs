621e9863a67ccd4f4c673eeb5d9efec82c6591a1
Alexander Christner <christner.alex@gmail.com>
2018-04-15 13:12:51 -0400

    added a level saver and continue button to menu. Requires testing

---
+++ Assets/SceneSaverScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSaverScript : MonoBehaviour {
+
+    string saved_scene;
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(this.gameObject);
+        saved_scene = null;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public string SavedScene
+    {
+        get
+        {
+            return saved_scene;
+        }
+        set
+        {
+            saved_scene = value;
+        }
+    }
+
+    public void ContinueFromLastLevel()
+    {
+        if(saved_scene != null)
+        {
+            SceneManager.LoadScene(saved_scene);
+        }
+    }
+}
