22626b2e6d38375ddad6bdc2524ea97d3be23a43
Aetreus <staeliam@gmailcom>
2019-02-27 19:11:51 -0600

    Initial script for data loading system.

---
+++ Assets/Scripts/DataLoader/TypeSpec.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[System.Serializable]
+public class TypeSpec{
+
+    public GameObject prefabBase;
+
+    public GameObject Instantiate()
+    {
+        return GameObject.Instantiate(prefabBase);
+    }
+
+    public GameObject Instantiate(Vector3 location, Quaternion rotation)
+    {
+        return GameObject.Instantiate(prefabBase, location, rotation);
+    }
+    
+    public GameObject Instantiate(Vector3 location, Quaternion rotation, Transform parent)
+    {
+        return GameObject.Instantiate(prefabBase, location, rotation, parent);
+    }
+}
